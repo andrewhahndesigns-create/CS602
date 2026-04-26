7649a20a3c6cdaaa7dcd1e5d2148017cf54d879b
samantha-osborne <109815208+samantha-osborne@users.noreply.github.com>
2026-04-26 11:25:02 -0400

    Update original_api_calls_retrieve_and_stored_data.py

--- original_api_calls_retrieve_and_stored_data.py
+++ original_api_calls_retrieve_and_stored_data.py
@@ -1,19 +1,51 @@
-import requests
-import json
+# import requests
+# import json
 
-"""
-# Browse the overall Asteroid data-set
-API_KEY = "DEMO_KEY"
-url = "https://api.nasa.gov/neo/rest/v1/neo/browse"
-params = {
-    "api_key": API_KEY
-}
-# requests.get() accepts params argument that takes a dictionary and append it to the url sent out.
-response = requests.get(url, params=params)
-print(response.json())
-neo_resp = response.json()
+# """
+# # Browse the overall Asteroid data-set
+# API_KEY = "DEMO_KEY"
+# url = "https://api.nasa.gov/neo/rest/v1/neo/browse"
+# params = {
+#     "api_key": API_KEY
+# }
+# # requests.get() accepts params argument that takes a dictionary and append it to the url sent out.
+# response = requests.get(url, params=params)
+# print(response.json())
+# neo_resp = response.json()
 
-w_file = open("neo_browse_data.json", "w")  # w- write
-json.dump(neo_resp, w_file, indent=2)  # write JSON
-w_file.close() 
-"""
+# w_file = open("neo_browse_data.json", "w")  # w- write
+# json.dump(neo_resp, w_file, indent=2)  # write JSON
+# w_file.close() 
+# """
+
+
+
+
+# [PY1] A function with two or more parameters,
+# one of which has a default value, called at least twice (once with the default value and once without)
+
+# Function to call api with default api-key and write response JSON to a file
+def get_api_response(url, json_file_name, api_key = "DEMO_KEY"):
+    import requests
+    import json
+
+    params = {
+        "api_key": api_key
+    }
+
+    # requests.get() accepts params argument that takes a dictionary and append it to the url sent out.
+    response = requests.get(url, params=params)
+
+    resp = response.json()
+    print(response.json())
+
+    w_file = open(json_file_name, "w")  # w- write
+    json.dump(resp, w_file, indent=2)  # write JSON
+    w_file.close()
+
+
+# Ran once for Near Earth Object 'Browse' Data
+get_api_response("https://api.nasa.gov/neo/rest/v1/neo/browse", "neo_browse_data.json", "DEMO_KEY")
+
+
+# And ran again for Natural Event Tracker Data
