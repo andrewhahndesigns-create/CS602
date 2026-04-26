92f0a66c2da20f4bfa2595e3055485756784856b
samantha-osborne <109815208+samantha-osborne@users.noreply.github.com>
2026-04-26 11:44:36 -0400

    Update original_api_calls_retrieve_and_stored_data.py

--- original_api_calls_retrieve_and_stored_data.py
+++ original_api_calls_retrieve_and_stored_data.py
@@ -1,29 +1,9 @@
-# import requests
-# import json
-
-# """
-# # Browse the overall Asteroid data-set
-# API_KEY = "DEMO_KEY"
-# url = "https://api.nasa.gov/neo/rest/v1/neo/browse"
-# params = {
-#     "api_key": API_KEY
-# }
-# # requests.get() accepts params argument that takes a dictionary and append it to the url sent out.
-# response = requests.get(url, params=params)
-# print(response.json())
-# neo_resp = response.json()
-
-# w_file = open("neo_browse_data.json", "w")  # w- write
-# json.dump(neo_resp, w_file, indent=2)  # write JSON
-# w_file.close() 
-# """
-
 
 # https://api.nasa.gov/
 '''
-NASA offers numerous free APIs, there is a rate limit of 50 requests per IP per day 
+NASA offers numerous free APIs, however, there is a rate limit of 50 requests per IP per day
 for the Demo Key. Group 1 decided to write a function to call the APIs, and then store the JSON
-repsonse packet in a file, so we could use that to query from
+repsonse packet in a file, so we could use that to query from for our streamlit web-application
 '''
 
 
@@ -43,7 +23,7 @@ def get_api_response(url, json_file_name, api_key = "DEMO_KEY"):
     response = requests.get(url, params=params)
 
     # [PY5] A dictionary where you write code to access its keys, values, or items
-    resp = response.json()
+    resp = response.json() # response is string in JSON format, .json() converts it to a python dictionary
     print(response.json())
 
     w_file = open(json_file_name, "w")  # w- write
@@ -52,7 +32,7 @@ def get_api_response(url, json_file_name, api_key = "DEMO_KEY"):
 
 
 # Ran once for Near Earth Object 'Browse' Data
-get_api_response("https://api.nasa.gov/neo/rest/v1/neo/browse", "neo_browse_data.json", "DEMO_KEY")
-
+get_api_response("https://api.nasa.gov/neo/rest/v1/neo/browse", "neo_browse_data.json")
 
 # And ran again for Natural Event Tracker Data
+get_api_response("https://eonet.gsfc.nasa.gov/api/v3/events", "natural_events_data.json", "")
