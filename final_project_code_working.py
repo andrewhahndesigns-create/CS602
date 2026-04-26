067ac6c63f002ffefa694a3d3db7ae873ed2c265
samantha-osborne <109815208+samantha-osborne@users.noreply.github.com>
2026-04-26 12:32:20 -0400

    Update final_project_code_working.py

--- final_project_code_working.py
+++ final_project_code_working.py
@@ -1,3 +1,25 @@
+"""
+GROUP 1: Andrew Hanh and Samantha Osborne
+DATA: We worked with 2 datasets from NASA:
+    -- Near Earth Object (Asteroids)
+    -- Natural Event Observational Data.
+
+DESCRIPTION:
+   Our Web App has three 'pages,' which one can explore from the radio sidebar options. 
+   
+   The landing page displays an image that changes daily (from NASA's 'Picture of the Day') API. 
+   We Cache the API reponse for one day after being called to display the image and caption using the st.image()
+
+   Then we have two data-driven pages, each deriving their data from JSON datasets that we retreived once 
+   and stored. The first dataset provides data on asteroids from NASA's Asteroid team. The second datset provides 
+   information on Natural Events observed in NASA imagery. 
+
+"""
+
+
+
+
+
 import requests
 import streamlit as st
 import pandas as pd
