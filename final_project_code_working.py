426785e4145a98ec871a875ef36d748722716457
samantha-osborne <109815208+samantha-osborne@users.noreply.github.com>
2026-04-26 11:10:05 -0400

    Create final_project_code_working.py

---
+++ final_project_code_working.py
@@ -0,0 +1,54 @@
+import requests
+import streamlit as st
+import pandas as pd
+
+############### Setting the background
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background-color: #000000;
+        color: #FFFFFF;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+###############
+st.sidebar.title("Welcome")
+sidebar_options = ['Astronomy Picture of the Day','Near Earth Objects','Earth Observatory Natural Event Tracker']
+page = st.sidebar.radio("Explore NASA Data", sidebar_options)
+
+if page == 'Astronomy Picture of the Day':
+    st.header("Astronomy Picture of The Day")
+
+    # function decorator; Wrap this function with Streamlit’s caching system
+    @st.cache_data(ttl=86400)
+    def get_apod():
+        API_KEY = "DEMO_KEY"
+        url = "https://api.nasa.gov/planetary/apod"
+
+
+        params = {
+            "api_key": API_KEY
+        }
+        # requests.get() accepts params argument that takes a dictionary and append it to the url sent out.
+        response = requests.get(url, params=params)
+
+        # print(response.json())
+        apod_resp = response.json()
+        return apod_resp
+
+
+    apod_response = get_apod()
+    # print(apod_response)
+    st.image(apod_response['url'], caption=f'{apod_response['date']} : {apod_response['explanation']}')
+
+
+elif page == 'Near Earth Objects':
+    st.header("Near Earth Objects \"Asteroids\"")
+
+
+elif page == 'Earth Observatory Natural Event Tracker':
+    st.header("Earth Observatory Natural Event Tracker")
