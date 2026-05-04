004348dc9dc1c3de90425de49ea45beb3828a132
samantha-osborne <109815208+samantha-osborne@users.noreply.github.com>
2026-05-04 08:58:40 -0400

    Create example_api_call.py

---
+++ example_api_call.py
@@ -0,0 +1,13 @@
+import requests
+
+
+url = "https://api.nasa.gov/neo/rest/v1/neo/browse"
+params = {
+    "api_key": "DEMO_KEY"
+}
+
+# requests.get() accepts params argument that takes a dictionary and append it to the url sent out.
+response = requests.get(url, params=params)
+
+resp = response.json()  # response is string in JSON format, .json() converts it to a python dictionary
+print(response.json())
